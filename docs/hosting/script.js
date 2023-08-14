f011750c437c31c25e164d4623cdfd27f9a7685a
AlonsoAliaga <alonsoaliagaa@gmail.com>
2023-08-14 11:45:01 -0500

    First release

---
+++ docs/hosting/script.js
@@ -0,0 +1,38 @@
+let times = 0;
+function loadCounter() {
+ let link = atob("aHR0cHM6Ly9hbG9uc29hbGlhZ2EtcGFnZS1jb3VudC5nbGl0Y2gubWUvY291bnRlcj9zaXRlPTxzaXRlPiZrZXk9PGtleT4=").replace(/<site>/g,"alonsoaliaga").replace(/<key>/g,"KEY-A");
+ let counter = document.getElementById("alonsohosting");
+ if(counter) {
+   $.ajax({
+     url: link,
+     type: "GET", /* or type:"GET" or type:"PUT" */
+     dataType: "json",
+     data: {
+     },
+     success: function (result) {
+       if(isNaN(result))
+         document.getElementById("counter-amount").innerHTML = "Click to return!";
+       else document.getElementById("counter-amount").innerHTML = `Visits: ${result}`;
+     },
+     error: function (e) {
+       times++;
+       document.getElementById("counter-amount").innerHTML = "Click to return!";
+       if(times <= 1) {
+        setTimeout(()=>{
+          loadCounter();
+        },1000*10);
+       }
+     }
+   });
+ }
+}
+function checkSite(window) {
+  return;
+  setTimeout(()=>{
+    let href = window.location.href;
+    console.log(href)
+    if(!href.includes(atob("YWxvbnNvYWxpYWdhLmdpdGh1Yi5pbw==")) || href.startsWith("file:")) {
+      try{document.title = `Page stolen from https://${atob("YWxvbnNvYWxpYWdhLmdpdGh1Yi5pbw==")}`;}catch(e){}
+      window.location = `https://${atob("YWxvbnNvYWxpYWdhLmNvbS9BbG9uc29Ib3N0aW5n")}/`}
+  },1000);
+}
